5a1d4c66f1813e9f6743bf58cf174ab293a0d826
Andrew <andrewmgunn26@gmail.com>
2021-09-12 14:15:04 -0500

    added other class for saving

---
+++ Program-2.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Threading.Channels;
+
+namespace Module4Assignment
+{
+    class Program_2
+    {
+        static void NumberSeries()
+        {
+            int startNumber = 0;
+            int endNumber = 0;
+            int oddNumbers = 0;
+            int evenNumbers = 0;
+            double evenCalc = 0;
+            double oddCalc = 0;
+
+
+            Console.Write("Starting Number>");
+            Int32.TryParse(Console.ReadLine(), out startNumber);
+
+
+            Console.Write("Ending Number>");
+
+            Int32.TryParse(Console.ReadLine(), out endNumber);
+
+            if (startNumber < endNumber)
+            {
+                Console.WriteLine($"Starting number: {startNumber}");
+                Console.WriteLine($"Ending number: {endNumber}");
+
+                while (startNumber <= endNumber)
+                {
+                    if (startNumber % 2 == 0)
+                    {
+                        evenNumbers++;
+                    }
+                    else
+                    {
+                        oddNumbers++;
+                    }
+                    
+                    
+                    
+                    startNumber++;
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Invalid: Start: {startNumber} must be LESS than End: {endNumber}");
+                if (startNumber % 2 == 0)
+                {
+                    evenNumbers++;
+                }
+                else
+                {
+                    oddNumbers++;
+                }
+
+                if (endNumber % 2 == 0)
+                {
+                    evenNumbers++;
+                }
+                else
+                {
+                    oddNumbers++;
+                }
+
+                Console.WriteLine($"Even numbers: {evenNumbers}");
+                Console.WriteLine($"Odd numbers: {oddNumbers}");
+            }
+            
+            Console.WriteLine($"Even numbers: {evenNumbers}");
+            Console.WriteLine($"Odd numbers: {oddNumbers}");
+        }
+
+        static void Main2(string[] args)
+        {
+            NumberSeries();
+        }
+    }
+}
