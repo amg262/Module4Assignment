40992291c399ab8724f8d49507d54e877c856025
Andrew <andrewmgunn26@gmail.com>
2021-09-12 13:40:38 -0500

    building out

--- Program.cs
+++ Program.cs
@@ -4,9 +4,32 @@ namespace Module4Assignment
 {
     class Program
     {
+
+        static void NumberSeries()
+        {
+            int startNumber = 0;
+            int endNumber = 0;
+
+            Console.WriteLine("Starting Number>");
+            Console.WriteLine("Ending Number>");
+            
+            Int32.TryParse(Console.ReadLine(), out startNumber);
+            Int32.TryParse(Console.ReadLine(), out endNumber);
+
+            if (startNumber < endNumber)
+            {
+                
+            }
+            else
+            {
+                Console.WriteLine("Error: Starting number must be LESS than ending number.");
+            }
+
+
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
